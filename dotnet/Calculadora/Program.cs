bde20b2f2bfdef6edb47633a660137f1cbcaf448
Enrico Bessa <86358593+EnricoBessa@users.noreply.github.com>
2024-03-13 13:44:25 -0300

    Firts update
    First update including all things are alredy done

---
+++ dotnet/Calculadora/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Console.WriteLine("------ SOMA ------");
+            Soma();
+            Console.WriteLine("------ SUBTRACAO ------");
+            Subtracao();
+            Console.WriteLine("------ MULTIPLICACAO ------");
+            Multiplicacao();
+            Console.WriteLine("------ DIVISAO ------");
+            Divisao();
+        }
+
+        static void Soma()
+        {
+            Console.WriteLine("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+            float total = valor1 + valor2;
+            Console.WriteLine("O primeiro valor " + valor1 + ", o segundo valor " + valor2);
+            Console.WriteLine($"Somando os dois totalizando {total}");
+            Console.ReadKey();
+        }
+        static void Subtracao()
+        {
+            Console.WriteLine("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+            float total = valor1 - valor2;
+            Console.WriteLine("O primeiro valor " + valor1 + ", o segundo valor " + valor2);
+            Console.WriteLine($"Subtraindo os dois {total}");
+            Console.ReadKey();
+        }
+        static void Multiplicacao()
+        {
+            Console.WriteLine("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+            float total = valor1 * valor2;
+            Console.WriteLine("O primeiro valor " + valor1 + ", o segundo valor " + valor2);
+            Console.WriteLine($"Multiplicando os dois {total}");
+            Console.ReadKey();
+        }
+        static void Divisao()
+        {
+            Console.WriteLine("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+            float total = valor1 / valor2;
+            Console.WriteLine("O primeiro valor " + valor1 + ", o segundo valor " + valor2);
+            Console.WriteLine($"Dividindo os dois {total}");
+            Console.ReadKey();
+        }
+    }
+
+}
+
+
---
+++ dotnet/MeuApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] agrs){
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float a = 0;
+        }
+    }
+
+}
+
+
